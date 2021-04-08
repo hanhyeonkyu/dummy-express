93daadbbf43d92ed8708aba276fff1240f871eea
hanhyeonkyu <busgod1212@gmail.com>
2021-04-08 15:08:31 +0900

    health-check

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import App from "./App";
 import express from "express";
 
-const port: number = Number(process.env.PORT) || 80;
+const port: number = Number(process.env.PORT) || 5000;
 const app: express.Application = new App().app;
 
 app
